301b26ff257ad4cf0a2cc42328b2fb18d6a14374
gustavo-de-assis <gustavo.siqueira.421@gmail.com>
2023-01-04 21:12:20 -0300

    feat: connection with backend application successfully working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./components/Main";
 import Post from "./components/Post";
 import GlobalStyle from "./styles/GlobalStyle";
 
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle/>
-      <Post/>    
-    </>
+      <Routes>
+        <Route path="/main" element={<Main/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
