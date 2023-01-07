e5025c3ab324e3d15525ad613178fa30454a6277
higorpr <higorpr@gmail.com>
2023-01-07 00:13:12 -0300

    bug:correcting merge error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import GlobalStyle from "./theme/globalStyles";
 import ProjectContext from "./constants/Context";
 import { useState } from "react";
 import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
+import SignUp from "./Components/SignUp";
 import Main from "./components/Main";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
 				<GlobalStyle />
 				<Routes>
 					<Route path="/" element={<LoginPage />} />
-					<Route path="/sign-up" element={<SignupPage />} />
+					<Route path="/sign-up" element={<SignUp />} />
 					<Route path="/timeline" element={<Main />} />
 				</Routes>
 			</BrowserRouter>
