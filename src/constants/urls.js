83c862635ee03e0cac9075fc197d6d3b20560408
ThaisFReis <reis.thaisf@gmail.com>
2023-01-08 21:21:38 -0300

    hashtagUrl adicionado de volta

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -4,4 +4,6 @@ export const logoutUrl = "http://localhost:4000/logout";
 
 export const signUpUrl = "http://localhost:4000/signup";
 
+export const hashtagUrl = "http://localhost:4000/hashtag:hashtag";
+
 export const usersLikedUrl = "http://localhost:4000/posts/likes";
