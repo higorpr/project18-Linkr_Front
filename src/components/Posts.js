7b9954e0e4470cc77321e01e94e38268226c3ebc
higorpr <higorpr@gmail.com>
2023-01-13 13:38:24 -0300

    bug: fix bug that would stop posts from loading

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -13,35 +13,8 @@ export function Posts(props) {
 	const { updatePosts } = props;
 	const [closeComment, setCloseComment] = useState(1);
 
-
 	// Using useCallback to avoid that the function is redefined every time the component is rendered
 	const getPosts = useCallback(() => {
-	function getFriends (){
-		const config = {
-			headers: {
-				authorization: `Bearer ${user.token}`,
-			},
-		};
-		let response;
-
-		axios.get(`http://localhost:4000/followeds`,config)
-		.then((ans)=>{
-
-			if(ans.status === 404){
-				response = false;
-			} else {
-				response = true;
-			}
-
-		})
-		.catch((err)=>{
-			console.log(err.data)
-		})
-		
-		return response;
-	}
-
-	function getPosts() {
 		setLoading(true);
 		const Url = `${process.env.REACT_APP_API_BASE_URL}/posts`;
 		const config = {
@@ -53,16 +26,11 @@ export function Posts(props) {
 			axios
 				.get(Url, config)
 				.then((answer) => {
+					console.log(answer);
 					setPost(answer.data);
 					setLoading(false);
-					if (answer.data.length === 0) {
-						if(getFriends()){
-							setError("No posts found from your friends");
-						}
-						else{
-							setError("You don't follow anyone yet. Search for new friends!");
-
-						} 
+					if (!answer.data.length) {
+						setError("There are no posts yet!");
 						alert("There are no posts yet");
 					} else {
 						setError("");
@@ -82,9 +50,10 @@ export function Posts(props) {
 		}
 	}, [user.token, updatePosts]);
 
+
 	useEffect(() => {
 		getPosts();
-	  }, [user, numberReloads, getPosts]);
+	}, [user, numberReloads, getPosts]);
 
 	return (
 		<>
